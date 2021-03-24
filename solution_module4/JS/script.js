79b63cf6924c0c833f75c60a5d01076e1962e991
Marco <marco_poli@outlook.de>
2021-03-24 18:35:05 +0100

    HTML Module Assingment 4

---
+++ solution_module4/JS/script.js
@@ -0,0 +1,17 @@
+var array = new Array(); 
+	array[0] = "Heidi";
+	array[1] = "Joe";
+	array[2] = "Kathrin";
+	array[3] = "Marco";
+	array[4] = "Lea";
+	array[5] = "John";
+	array[6] = "Peter";
+	array[7] = "Lisa";
+	array[8] = "Don";
+	array[9] = "Marta";
+
+console.log(array);
+
+for (var i = 0; i < array.length; i++) {
+	console.log("Hello " + array[i] + "!");
+};
